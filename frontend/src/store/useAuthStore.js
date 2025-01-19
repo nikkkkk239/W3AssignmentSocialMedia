fd45775558b002248336e071fe60e5bb367eb7b7
nikkkkk239 <nikhilpanday239@gmail.com>
2025-01-19 11:02:27 +0530

    Removed Socket error.

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -3,7 +3,7 @@ import { axiosInstance } from "../lib/axiosInstance";
 import {toast} from "react-hot-toast";
 import { io } from "socket.io-client";
 
-const BASE_URL = import.meta.env.VITE_REACT_APP_BASEURL ;
+const BASE_URL = import.meta.env.VITE_REACT_APP_BASEURL;
 
 export const useAuthStore = create((set,get)=>({
     authUser : null,
