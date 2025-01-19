11daaf2fb77bc6d711c174d6c65fa0a0452f6520
nikkkkk239 <nikhilpanday239@gmail.com>
2025-01-19 11:07:41 +0530

    Updated socket.js

--- backend/lib/socket.js
+++ backend/lib/socket.js
@@ -2,9 +2,11 @@ import {Server} from "socket.io"
 import http from 'http'
 import express from "express"
 import dotenv from "dotenv"
+import cors from "cors"
 dotenv.config();
 
 const app = express();
+
 const server = http.createServer(app);
 
 const io = new Server(server,{
